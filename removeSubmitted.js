39dd0490f98c5834d315cd00bfc5bffecea86b5b
Tristan Trim <TristanTrim@gmail.com>
2023-07-10 19:59:06 -0700

    remove sub now removes from iframe

--- removeSubmitted.js
+++ removeSubmitted.js
@@ -1,6 +1,7 @@
 
 // hide all but the unsubmitted assignments
 
+// for main page
 list = document.getElementsByClassName("d_gt");
 for (let i=list.length; i>=0; i=i-1){
   let td = list[i];
@@ -10,4 +11,20 @@ for (let i=list.length; i>=0; i=i-1){
       ){
          td.parentElement.hidden=true;
   }
+}
+
+// for iframe pages
+for (const ifr of document.getElementsByTagName("iframe")){
+  
+  list = ifr.contentWindow.document.getElementsByClassName("d_gt");
+  for (let i=list.length; i>=0; i=i-1){
+    let td = list[i];
+    console.log(td);  
+    if( td && td.className == "d_gt"
+          && ! td.firstChild.innerText.startsWith("Not Sub")
+        ){
+           td.parentElement.hidden=true;
+    }
+  }
+	ifr.height = ifr.contentWindow.document.body.scrollHeight;
 }
